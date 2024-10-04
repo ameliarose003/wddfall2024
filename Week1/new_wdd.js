5e6f60343456373d0143057afd1ab6d73db7339d
ameliarose003 <ameliarose003@byui.edu>
2024-10-03 18:12:49 -0600

    finished part 1 cool pics

---
+++ Week1/new_wdd.js
@@ -0,0 +1,7 @@
+const myCookies = 3;
+const friendCookies = 3;
+let total = myCookies + friendCookies;
+
+document.getElementById("myCookiesElement").textContent = myCookies;
+document.getElementById("friendCookiesElement").textContent = friendCookies;
+document.getElementById("totalCookiesElement").textContent = total;
